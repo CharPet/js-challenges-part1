2084668f23c01b09d17124f00a0c1fca1fc50905
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-06 16:18:39 +0200

    Added files for Nike shoe challenge

--- script.js
+++ script.js
@@ -16,3 +16,40 @@ function greeter() {
 greeter();
 
 // --------------------
+
+let myPoints = 3;
+
+function add3Points() {
+  myPoints += 3;
+}
+
+function remove1Point() {
+  myPoints -= 1;
+}
+
+add3Points();
+// console.log(myPoints);
+
+add3Points();
+// console.log(myPoints);
+
+add3Points();
+// console.log(myPoints);
+
+remove1Point();
+// console.log(myPoints);
+
+remove1Point();
+
+console.log(myPoints);
+
+// --------------------
+
+console.log("2" + 2); // 22
+console.log(11 + 7); // 18
+console.log(6 + "5"); // 65
+console.log("My points: " + 5 + 9); // My points: 59
+console.log(2 + 2); // 4
+console.log("11" + "14"); // 1114
+
+// --------------------
