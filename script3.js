31562f12e05eb9debbf6742c6318648806541357
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-06 16:54:08 +0200

    Created files for Basketball Scoreboard

--- script3.js
+++ script3.js
@@ -2,3 +2,21 @@ let num1 = 8;
 let num2 = 2;
 document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
+let sumEl = document.getElementById("sum-el");
+
+function add() {
+  let result = num1 + num2;
+  sumEl.textContent = "Sum: " + result;
+}
+
+function subtract() {
+  sumEl.textContent = "Sum: " + (num1 - num2);
+}
+
+function divide() {
+  sumEl.textContent = "Sum: " + num1 / num2;
+}
+
+function multiply() {
+  sumEl.textContent = "Sum: " + num1 * num2;
+}
