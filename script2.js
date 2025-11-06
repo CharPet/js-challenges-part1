44624b95e24abc89e29d2fb10da625c955ae3398
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-06 16:33:20 +0200

    Added files for Calculator challenge

---
+++ script2.js
@@ -0,0 +1,6 @@
+let purchaseButton = document.getElementById("purchase-button");
+let error = document.getElementById("error");
+
+function errorMessage() {
+  error.textContent = "Something went wrong, please try again!";
+}
