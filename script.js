8c438a4ca8cd13c5af4f1f0902ef6dee87de231e
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-06 16:00:41 +0200

    Init

---
+++ script.js
@@ -0,0 +1,18 @@
+let firstName = "Pepe";
+let lastName = "Peperino";
+let fullname = firstName + " " + lastName;
+
+console.log(fullname);
+
+// --------------------
+
+let name = "Linda";
+let greeting = "Hi there";
+
+function greeter() {
+  console.log(greeting + ", " + name + "!");
+}
+
+greeter();
+
+// --------------------
